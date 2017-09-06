b880eee60fa95ae536bdbe36a5c44f4f34ddbea9
Alexander Kozhaev <alekzoid@gmail.com>
2017-09-06 14:08:42 +1000

    add numer devariatives

--- common/models/kelvin_fought/tension/numeric/__init__.py
+++ common/models/kelvin_fought/tension/numeric/__init__.py
@@ -1,37 +1,75 @@
 import multiprocessing as mp
 
-import numpy as np
-
 from common.models.kelvin_fought.deflection.numeric import deflection_func as w, integrator_adapter
 
-
-def f1(a):
-    return - a['l'] ** 2 * w(a)
+from cmath import *
 
 
-def f2(a):
-    return - a['e'] ** 2 * w(a)
+def d2w_dx2_dt(a):
+    return -1j * a['l'] ** 3 * a['v'] * dw_dt(a)
+
+def d2w_dy2_dt(a):
+    return -1j * a['l'] ** 3 * a['v'] * dw_dt(a)
 
 
-def f3(a):
-    return a['l'] * a['e'] * w(a)
 
+def dw_dt(a):
+    k = sqrt(a['l'] ** 2 + a['e'] ** 2)
+    C = sin(a['e'] * a['a']) * sin(a['l'] * a['b'])
+    phi = a['e'] * a['y'] + a['l'] * (a['x'] - a['v'] * a['t'])
+    delta = exp(-1j * phi)
+    p = 1j * a['freq']
+    K = k * tanh(a['H'] * k)
+    k2 = a['h'] * a['p_i'] + a['p_w'] / K
+    k1 = a['D'] * a['t_f'] * k ** 4 + 4j * a['l'] * a['v'] * k2
 
-def f4(a):
-    return - a['l'] ** 3 * a['v'] * w(a)
+    n = 1 / k2
+    l = k1 / k2
+    e_r_t = exp(-l * a['t'])
+    e_p_t = exp(p * a['t'])
 
+    w0_a = l * (1 - e_p_t) + p * (1 - e_r_t)
+    w0_a *= - 2 * n
+    w0_a /= l * p * (l + p)
 
-def f5(a):
-    return - a['e'] ** 2 * a['v'] * w(a)
+    w0_b = l ** 2 * (p * a['t'] + 1 + e_p_t) + p ** 2 * (l * a['t'] - 1 + e_r_t)
+    w0_b *= n
+    w0_b /= l ** 2 * p * (l + p)
+    w0 = w0_a + w0_b
 
+    w1_a = (e_p_t - e_r_t)
+    w1_a *= 2 * n
+    w1_a /= l + p
 
-def f6(a):
-    return - a['l'] ** 2 * a['v'] * w(a)
+    w1_b = l * (1 - e_p_t) + p * (1 - e_r_t)
+    w1_b *= n
+    w1_b /= l * (l + p)
+    w1 = w1_a + w1_b
+
+    w2_a = (l * e_r_t + p * e_p_t)
+    w2_a *= 2 * n
+    w2_a /= l + p
+
+    w2_b = p * (e_p_t - e_r_t)
+    w2_b *= -n
+    w2_b /= (l + p)
+
+    w2 = w2_a + w2_b
+
+    denom = a['D'] * k ** 4 + a['g'] * a['p_w'] - a['l'] ** 2 * a['v'] ** 2 * (
+        k2) + 1j * a['D'] * a['t_f'] * a['l'] * a['v'] * (k ** 4)
+
+    w = (1 + k1 * w1 + k2 * w2) / denom
+    w *= a['P_max'] / 2
+    value = w * delta * C / a['l'] / a['e']
+    return value.real
 
 
 def dmx(a):
-    return a['D'] * (f1(a) + a['mu'] * f2(a) + a['t_f'] * (f4(a) + a['mu'] * f5(a))) * 16 * a['P'] / (np.pi ** 2) / (
-        4 * a['a'] * a['b'])
+    w_val = w(a)
+    f2 = -a['e'] ** 2 * w_val
+    f4 = -1j * a['l'] ** 3 * a['v'] * w_val
+    return - a['D'] * (f1(a) + a['mu'] * f2 + a['t_f'] * (f4 + a['mu'] * f5))
 
 
 def dmy(a):
